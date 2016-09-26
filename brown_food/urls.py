891aa3dc1aea950c4574f6235851696187c55102
Wanyama Harold <wanyaland@gmail.com>
2016-09-26 12:29:32 +0300

    menu changes

--- brown_food/urls.py
+++ brown_food/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
 
 urlpatterns = patterns('',
@@ -6,5 +7,5 @@ urlpatterns = patterns('',
     # url(r'^$', 'brown_food.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
                        url(r'^',include('core.urls',namespace='core')),
-                       url(r'^admin/',include('admin.urls',namespace='admin')),
+                       url(r'^admin/',include(admin.site.urls)),
 )
