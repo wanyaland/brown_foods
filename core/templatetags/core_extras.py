3000592eff50f3b05a39eefa8b13b11180e0aa6b
Wanyama Harold <wanyaland@gmail.com>
2016-09-27 16:22:40 +0300

    changes on 9/27/2016

--- core/templatetags/core_extras.py
+++ core/templatetags/core_extras.py
@@ -17,3 +17,14 @@ def in_cart_session(code,cart):
 def split(price):
     str_price = str(price)
     return str_price[:-3]
+
+@register.filter
+def next_el(some_list, current_index):
+    """
+    Returns the next element of the list using the current index if it exists.
+    Otherwise returns an empty string.
+    """
+    try:
+        return some_list[int(current_index) + 1] # access the next element
+    except:
+        return '' # return empty string in case of exception
